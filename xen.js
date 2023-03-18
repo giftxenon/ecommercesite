ada0ca9e7bec99448095021ba60ca8a41e33f4c2
SSEMMAMBO GIFT BENON <106618273+giftxenon@users.noreply.github.com>
2023-03-18 11:23:16 +0300

    Update xen.js
    the print Hello world JavaScript

--- xen.js
+++ xen.js
@@ -3,3 +3,16 @@ var name = "xenon";
 document.write(name);
 console.log('Hello, World!');
 prompt()
+<html>  
+
+<body>  
+
+<script type="text/javascript">  
+
+ alert("Hello World");  
+
+</script>  
+
+</body>  
+
+</html>
