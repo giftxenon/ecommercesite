c444d9b79552b0d16e93fe87df2f6860d565549c
SSEMMAMBO GIFT BENON <106618273+giftxenon@users.noreply.github.com>
2023-03-19 23:21:51 +0300

    Update xen.js
    an addittionof arrays and accessing them

--- xen.js
+++ xen.js
@@ -16,6 +16,16 @@ var colors = ["Red", "Green", "Blue"];
 var fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
 var cities = ["London", "Paris", "New York"];
 var person = ["John", "Wick", 32];
+var cars = ["BMW", "Toyota", "Audi", "Benz", "Isuzu", "RangeRover", "Mercedes", "Jeep", "limousine"];
+
+
+//Accessing the elements of an aray 
+document.write(fruits[0]); // Prints: Apple
+document.write(fruits[1]); // Prints: Banana
+document.write(fruits[2]); // Prints: Mango
+document.write(fruits[3]); // prints: orange
+document.write(cars[0]); //prints: BMW
+document.write(cars[4]); //prints:Isuzu
 
 </script>  
 
