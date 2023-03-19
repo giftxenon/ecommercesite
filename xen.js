9dcfd214230294eccdaa33fbcae000ae1821a832
SSEMMAMBO GIFT BENON <106618273+giftxenon@users.noreply.github.com>
2023-03-19 23:12:32 +0300

    Update xen.js
    adding arrays in js

--- xen.js
+++ xen.js
@@ -9,7 +9,13 @@ prompt()
 
 <script type="text/javascript">  
 
- alert("Hello World");  
+ alert("Hello World"); 
+// how to declare arrays in javascript
+
+var colors = ["Red", "Green", "Blue"]; 
+var fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+var cities = ["London", "Paris", "New York"];
+var person = ["John", "Wick", 32];
 
 </script>  
 
